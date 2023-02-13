3b92080a16528c8703edc75737031a128f4c7987
Colin Greeley <37784147+ColinGreeley@users.noreply.github.com>
2023-02-13 15:42:14 -0800

    Update train.py

--- Training/train.py
+++ Training/train.py
@@ -6,7 +6,7 @@ Usage: python3 train.py --tile_dir --tissue_type --pathologies --[tile_size] --[
 Used to train tile classifying models. The arguments to the script
 are as follows:
 - <tile_dir> Path to directory of tiles for training
-- <tissue_type> help='Name of the tissue type. i.e., "breast" tissue
+- <tissue_type> Name of the tissue type. i.e., "breast" tissue
 - <pathologies> Names of the pathologies you want to classify
 - [<tile_size>] (Optional) Resolution of tiles used for neural network input
 - [<batch_size] (Optional) Batch size for training neural networks
@@ -16,11 +16,12 @@ Authors: Colin Greeley and Larry Holder, Washington State University
 """
 
 import tensorflow as tf
-from image_classifier import Classifier
+from image_classifier_SA import Classifier
 import argparse
 import pandas as pd
 import numpy as np
-import cv2
+import os
+from skimage.io import imread, imsave
 
 
 def get_data(base_dir, classes, tile_size):
@@ -28,23 +29,25 @@ def get_data(base_dir, classes, tile_size):
     df = pd.read_csv(base_dir + ('tiles.csv' if base_dir[-1] == '/' else '/tiles.csv'))
     images = []
     labels = []
-    for image_name, ps in zip(df["image"], df["pathology"]):
+    for image_name, x, y, width, height, ps in zip(df['image'], df['x'], df['y'], df['width'], df['height'], df["pathology"]):
+        #if "#017" in image_name:
         p_list = [p.lower() for p in ps.split(' ')]
         if not ("ignore" in p_list and "ignore" not in classes):
-            images.append(cv2.resize(cv2.imread(image_dir + image_name + '.png'), (tile_size, tile_size), interpolation=cv2.INTER_AREA))
-            label = [0 for _ in range(len(classes)+1)]
-            for pathology in p_list:
-                if pathology in classes:
-                    label[classes.index(pathology)] = 1
-                else:
-                    label[-1] = 1
-            labels.append(label)
+            if True: #"B3" not in image_name:
+                images.append(("".join(image_name.split('_')[:-1]), x, y, width, height))
+                label = [0 for _ in range(len(classes)+1)]
+                for pathology in p_list:
+                    if pathology.lower() in classes:
+                        label[classes.index(pathology)] = 1
+                    else:
+                        label[-1] = 1
+                labels.append(label)
     images = np.array(images)
     labels = np.array(labels)
     for i in range(len(classes)):
         print(classes[i] + " images:", np.sum(labels[:,i]))
     print("negative class images:", np.sum(labels[:,len(classes)]))
-    return images, labels
+    return images, labels, set(df['image'])
 
 
 def get_args():
@@ -53,8 +56,9 @@ def get_args():
     parser.add_argument('--tissue_type', type=str, required=True, help='Name of the tissue type. i.e., "breast" tissue')
     parser.add_argument('--pathologies', nargs="*", type=str, required=True, help='Name of the pathologies you want to classify. They will be the positive classes for new multiclass classification model. Every other class will be treated as the negative class.')
     parser.add_argument('--tile_size', type=int, required=False, default=256, help='Resolution of tiles used for neural network input')
-    parser.add_argument('--batch_size', type=int, required=False, default=50, help='Batch size for training neural networks')
-    parser.add_argument('--ensemble_size', type=int, required=False, default=5, help='Number of classifiers in ensemble')
+    parser.add_argument('--batch_size', type=int, required=False, default=77, help='Batch size for training neural networks')
+    parser.add_argument('--ensemble_size', type=int, required=False, default=7, help='Number of classifiers in ensemble')
+    parser.add_argument('--overlap', type=str, required=False, default=30, help='Tile overlap required for tile to be classifier as positive class')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -67,6 +71,8 @@ if __name__ == "__main__":
     tile_size = args.tile_size
     batch_size = args.batch_size
     ensemble_size = args.ensemble_size
+    overlap = args.overlap
+    image_dir = "./qupath/" + tissue_type + "/tif/"
     if '/' in tissue_type:
         tissue_type = tissue_type.replace('/', '-')
         
@@ -74,8 +80,16 @@ if __name__ == "__main__":
     print("Available GPUs:", len(physical_devices))
     print("Tissue type:", tissue_type)
     print("Pathologies:", pathologies)
-        
-    images, labels = get_data(base_dir, pathologies, tile_size)
+    
+    image_data, labels, image_names = get_data(base_dir, pathologies, tile_size)
+    images = {}
+    for d in os.listdir(image_dir):
+        if True: #"B3" not in os.path.join(image_dir, d):
+            if os.path.splitext(d)[-1] == ".tif":
+                new_im = np.pad(imread(os.path.join(image_dir, d)), ((1000, 1000), (1000, 1000), (0,0)), 'constant', constant_values=0)
+                images.update({os.path.splitext(d)[0]: new_im})
+            
+            
     classifier = Classifier(image_rez=(tile_size,tile_size), batch_size=batch_size, ensemble_size=ensemble_size)
-    classifier.name = tissue_type + str(tile_size) + '_classifier'
-    classifier.train(images, labels)
+    classifier.name = tissue_type + str(tile_size) + '_classifier_SA_' + str(overlap)
+    classifier.train((images, image_data), labels)
