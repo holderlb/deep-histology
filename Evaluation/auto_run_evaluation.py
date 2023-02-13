6f90d48644b3d19af18ed275a88c2dee0b9790f8
Colin Greeley <37784147+ColinGreeley@users.noreply.github.com>
2023-02-13 15:17:59 -0800

    Update auto_run_evaluation.py
    Updated evaluation to support spatial awareness model

--- Evaluation/auto_run_evaluation.py
+++ Evaluation/auto_run_evaluation.py
@@ -3,7 +3,7 @@
 # Usage: python3 auto_run_heatmap_gen.py <src_dir>
 #
 #
-# Runs the generate_heatmap.py on all images in the folder tree createcreated by convert_tiff.py
+# Runs the evaluate.py on all images in the folder tree createcreated by convert_tiff.py
 # - <src_dir> source directory, path to the directory that contains the Control folder
 #
 # Authors: Colin Greeley and Larry Holder, Washington State University
@@ -20,13 +20,11 @@ if len(sys.argv) > 2:
 else:
     tissue_type = None
 
-data_map = {"testis": {"colors": "../qupath/testis/tiles/colors.json", "pathologies": ["Ignore", "Atrophy", "Maturation_arrest", "Vacuole"], "tissue_type": "testis"},
-            "prostate": {"colors": "../qupath/prostate/tiles/colors.json", "pathologies": ["Ignore", "Atrophy", "Collapsed_Prost", "Hyperplasia_Prost", "Vacuole"], "tissue_type": "prostate"},
-            "female kidney": {"colors": "../qupath/kidney/female/tiles/colors.json", "pathologies": ["Ignore", "Cyst", "Reduced_Glomeruli", "Thickened_Bowmans_Capsule"], "tissue_type": "kidney/female"},
-            "male kidney": {"colors": "../qupath/kidney/male/tiles/colors.json", "pathologies": ["Ignore", "Cyst", "Reduced_Glomeruli", "Thickened_Bowmans_Capsule"], "tissue_type": "kidney/male"},
-            "ovary": {"colors": "../qupath/ovary/tiles/colors.json", "pathologies": ["Ignore", "Antral_follicle", "Antral_follicle_wo_oocyte_ov", "Large_cyst_ov", "Preantral_follicle_ov", "Primordial_follicle", "Small_cyst_ov"], "tissue_type": "ovary"}
-            # any other pathologies can be added to this data map
-            }
+data_map = {"testis": {"colors": "../qupath/testis/tiles/colors.json", "pathologies": ["Ignore", "Atrophy", "Vacuole", "Normal_Tissue"], "tissue_type": "testis"},
+            "prostate": {"colors": "../qupath/prostate/tiles/colors.json", "pathologies": ["Ignore", "Atrophy", "Collapsed_Prost", "Hyperplasia_Prost", "Vacuole", "Normal_Tissue"], "tissue_type": "prostate"},
+            "female kidney": {"colors": "../qupath/kidney/female/tiles/colors.json", "pathologies": ["Ignore", "Cyst", "Reduced_Glomeruli", "Thickened_Bowmans_Capsule", "Normal_Tissue"], "tissue_type": "kidney/female"},
+            "male kidney": {"colors": "../qupath/kidney/male/tiles/colors.json", "pathologies": ["Ignore", "Cyst", "Reduced_Glomeruli", "Thickened_Bowmans_Capsule", "Normal_Tissue"], "tissue_type": "kidney/male"},
+            "ovary": {"colors": "../qupath/ovary/tiles/colors.json", "pathologies": ["Ignore", "Antral_follicle", "Large_cyst_ov", "Preantral_follicle_ov", "Primordial_follicle", "Small_cyst_ov", "Normal_Tissue"], "tissue_type": "ovary"}}
 
 
 def DFS_generate_heatmap(path, tissue_type):
@@ -35,7 +33,7 @@ def DFS_generate_heatmap(path, tissue_type):
         if os.path.isfile(os.path.join(path, dir)) and os.path.splitext(dir)[1] == '.tif':
             src_image_path = os.path.join(path, dir)
             print(src_image_path, tissue_type)
-            bashCommand = ["python3", "evaluate.py", 
+            bashCommand = ["python3", "evaluate_SA.py", 
                             "--image", src_image_path, 
                             "--pathology_colors", data_map[tissue_type.lower()]["colors"],
                             "--tissue_type", data_map[tissue_type.lower()]["tissue_type"],
@@ -48,7 +46,7 @@ def DFS_generate_heatmap(path, tissue_type):
                     print(line, end =" ")
             #if os.path.exists(os.path.split(src_image_path)[-1]) and os.path.isdir(os.path.split(src_image_path)[-1]):
             os.remove(src_image_path)
-        else:
+        elif os.path.isdir(os.path.join(path, dir)):
             if dir.lower() in data_map.keys():
                 tissue_type = dir.lower()
             DFS_generate_heatmap(os.path.join(path, dir), tissue_type)
